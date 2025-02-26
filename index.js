cb9d721da54f0e98f12d2c4cb1aa9a70bc2df33a
Toufikul Islam <imranmeg2356@gmail.com>
2025-02-26 21:24:48 +0600

    jdghfds

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ const { Server } = require("socket.io");
 const app = express();
 const port = process.env.PORT || 5000;
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://flybook.com.bd', 'http://localhost:5173'] // আপনার অনুমোদিত ডোমেইন
+}));
 app.use(express.json());
 
 // MongoDB connection URI
