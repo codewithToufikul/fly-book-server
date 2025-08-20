527a9071b0e5c320b4550dccfa275e91c0dda8b4
codewithToufikul <imranmeg2356@gmail.com>
2025-08-21 00:49:20 +0600

    jdsh

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { timeStamp } = require("console");
 const { translate } = require("@vitalets/google-translate-api");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 // Middleware
 app.use(
   cors({
@@ -55,9 +55,9 @@ async function connectToMongo() {
 }
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAMe,
+  api_key: process.env.CLOUDINARY_API_KEy,
+  api_secret: process.env.CLOUDINARY_API_SECREt,
 });
 
 const storage = new CloudinaryStorage({
