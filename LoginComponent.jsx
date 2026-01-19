53975444c3880b5c60006657b99e68386373ff52
codewithToufikul <imranmeg2356@gmail.com>
2026-01-19 16:10:46 +0600

    clean file structure

--- LoginComponent.jsx
+++
@@ -1,9 +0,0 @@
-const handleLogin = async (loginData) => {
-  try {
-    const response = await axios.post('/users/login', loginData);
-    const { token } = response.data;
-    localStorage.setItem('token', token); // Store token after successful login
-  } catch (error) {
-    console.error('Login error:', error);
-  }
-}; 
